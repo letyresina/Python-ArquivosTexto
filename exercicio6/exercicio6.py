3897d495ceb4a8176e3a9c3b5dae5f68b3a5b961
letyresina <leticiaresina@gmail.com>
2023-10-06 11:44:36 -0300

    terminar exercicio 6 em casa

---
+++ exercicio6/exercicio6.py
@@ -0,0 +1,6 @@
+'''
+    Exercício 6:
+    Um professor armazena em um arquivo de texto uma listagem das notas dos alunos na disciplina, 
+    onde cada linha contém o nome do aluno e os valores de quatro notas.
+    Escreva um programa que leia esse arquivo e calcule a média de cada aluno.
+'''
