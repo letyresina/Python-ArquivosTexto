1bcdcacf217e07cb82ee2be60a5ca057276f26a4
letyresina <leticiaresina@gmail.com>
2023-10-17 08:54:01 -0300

    subindo exercício 6

--- exercicio6/exercicio6.py
+++ exercicio6/exercicio6.py
@@ -4,3 +4,15 @@
     onde cada linha contém o nome do aluno e os valores de quatro notas.
     Escreva um programa que leia esse arquivo e calcule a média de cada aluno.
 '''
+
+with open('exercicio6/notas.txt', 'r', encoding='utf-8') as arquivo:
+    for linha in arquivo:
+        lista = linha.split()
+        notas = lista[-1:-5:-1]
+        print('-' * 50)
+        print(f'\nNotas: {notas}')
+        soma = 0
+        for n in notas:
+            soma += float(n)
+        media = soma / 4
+        print(f'{media:.2f}')
